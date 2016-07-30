a05fdd9ae7e88cc35a8c26bdd407448de0da2676
manuel <molop3@gmail.com>
2016-07-29 19:09:32 -0500

    maqueta parte 2

--- js/functions.js
+++ js/functions.js
@@ -1,12 +1,27 @@
+$(window).load(function(){
+	$(function() {
+
+		$("a.menu").click(function(){
+			$(".overscreen").show();
+			setTimeout(function() {$(".overscreen").addClass('active');}, 100);
+		});
+
+		$("div.fitted").click(function(){
+			$(".overscreen").removeClass('active');
+			setTimeout(function() {$(".overscreen").hide();}, 600);
+		});
+
+	});
+
+});
+
 (function($){
 
 	"use strict";
 
 	$(function(){
 
-
 		console.log('hello from functions.js');
-
 
 		/**
 		 * Validación de emails
@@ -15,8 +30,6 @@
 			var regExp = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 			return regExp.test(email);
 		};
-
-
 
 		/**
 		 * Regresa todos los valores de un formulario como un associative array 
@@ -30,7 +43,6 @@
 			});
 			return result;
 		}
-
 
 	});
 
